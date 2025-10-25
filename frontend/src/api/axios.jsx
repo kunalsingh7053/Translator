36598e709dedaabf824bcd448c9c105a6c63f979
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-10-25 17:32:16 +0530

    update backend api

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // backend base URL
+  baseURL: "https://translator-lo1e.onrender.com/api", // backend base URL
   withCredentials: true, // send cookies automatically
 });
 
