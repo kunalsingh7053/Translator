7d5855e46535d5ef9694620a8d5bd0033ff348cf
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-10-29 22:49:14 +0530

    fix redirect

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,16 @@
-// backend/src/app.js
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 
-// Routes
 const authRoutes = require("./routes/auth.routes");
 const translatorRoutes = require("./routes/chat.routes");
 const bookmarkRoutes = require("./routes/bookmark.routes");
 
 const app = express();
 
-// ------------------ 1️⃣ Middleware ------------------
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: ["https://translator-1-sa98.onrender.com", "http://localhost:5173"],
@@ -23,16 +19,14 @@ app.use(
   })
 );
 
-// ------------------ 2️⃣ API Routes ------------------
 app.use("/api/auth", authRoutes);
 app.use("/api/translator", translatorRoutes);
 app.use("/api", bookmarkRoutes);
 
-// ------------------ 3️⃣ Serve Frontend ------------------
-const __dirname1 = path.resolve(); // root of project
+const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname1, "frontend", "dist", "index.html"));
 });
 
