ddb9771276a780650f926669d3c1084668414d3c
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-11-29 15:06:49 +0530

    update

--- backend/src/service/email.service.js
+++ backend/src/service/email.service.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+    host: "smtp.gmail.com",
  auth: {
   user: process.env.EMAIL_USER,
   pass: process.env.EMAIL_PASS
